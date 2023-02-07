85664b8246509c05f0e02c875e2b77b34caa30ad
AndyDevla <pandax.777@hotmail.com>
2023-02-06 21:27:31 -0600

    added logical files and images from pixabay

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web-Image-Viewer
-A simple and real serverless album viewer to show all the images inside a folder in a webpage.
+A simple and real serverless album viewer to show as a webpage all the images inside a folder.
---
+++ folder_contents.js
@@ -0,0 +1,18 @@
+var g_FOLDER_CONTENTS = mlString(function() { /*! 
+air-balloon-g739268cd7_640.jpg
+birds-g496f3c311_1280.jpg
+landscape-g7f1cf3156_640.jpg
+man-g7c8150b5d_640.png
+moon-gc8bd76bcf_640.jpg
+motorbike-gee81cc122_640.jpg
+peron-gece8bf1d5_1280.jpg
+phone-wallpaper-g46c7674c7_640.jpg
+road-gebc4585ca_640.jpg
+sea-g9efe7bf9d_640.jpg
+tinner-g930421f95_640.jpg
+trees-gc06de30f0_640.jpg
+wall-g9923d87e1_640.jpg
+water-gb70625a28_1280.jpg
+waterfall-g2999d9c10_640.jpg
+zern-gf494c0c5f_640.jpg
+*/}); 
